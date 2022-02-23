5d4e34fc00db70fd391aabc35a13828369599ac7
mjheid <m.j.heidrich19@gmail.com>
2022-02-23 15:16:39 +0100

    add glorot init

--- dca/models.py
+++ dca/models.py
@@ -6,6 +6,10 @@ def MeanAct(x):
 def DispAct(x):
     return torch.clamp(torch.nn.Softplus(x), 1e-4, 1e4)
 
+def init_weights(layer):
+    if isinstance(layer, torch.nn.Linear):
+        torch.nn.init.xavier_uniform(layer.weight)
+
 class AutoEncoder(torch.nn.Module):
     def __init__(self, input_size, encoder_size, bottleneck_size):
         super().__init__()
@@ -28,6 +32,10 @@ class AutoEncoder(torch.nn.Module):
             torch.nn.Linear(self.bottleneck_size, self.encoder_size),
             torch.nn.ReLU()
             )
+        
+        self.encoder.apply(init_weights)
+        self.decoder.apply(init_weights)
+        self.bottleneck.apply(init_weights)
     
     def forward(self, x):
         x = self.encoder(x)
@@ -50,6 +58,9 @@ class NBAutoEncoder(AutoEncoder):
             torch.nn.Linear(self.encoder_size, self.input_size),
             DispAct()
             )
+        
+        self.mean.apply(init_weights)
+        self.disp.apply(init_weights)
     
     def forward(self, x):
         x = super().forward(x)
@@ -80,6 +91,10 @@ class ZINBAutoEncoder(torch.nn.Module):
             torch.nn.Linear(self.encoder_size, self.input_size),
             torch.nn.Sigmoid()
             )
+        
+        self.mean.apply(init_weights)
+        self.disp.apply(init_weights)
+        self.drop.apply(init_weights)
     
     def forward(self, x):
         x = super().forward(x)
